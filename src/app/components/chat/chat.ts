5b268f502f636989939d865334ca3f5107e45151
MaleesA28 <mariyammalys@gmail.com>
2025-09-04 17:09:34 +1000

    display current user in chat page

--- src/app/components/chat/chat.ts
+++ src/app/components/chat/chat.ts
@@ -9,12 +9,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class Chat implements OnInit {
   channelName: string | null = null;
+  currentUser: any = null; // to hold current user info
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     // Get the channel name from the URL
     this.channelName = this.route.snapshot.paramMap.get('name');
-    console.log('Current channel:', this.channelName);
+
+    const raw = localStorage.getItem('currentUser');
+    if (raw) {
+      this.currentUser = JSON.parse(raw);
+    }
   }
 }
